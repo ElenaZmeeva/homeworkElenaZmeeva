0cd4337c201fd554fcf2c00d53018f363d4aad7d
elenazmeeva <biertli@bk.ru>
2024-11-30 22:41:51 +0300

    first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,2 +1,8 @@
-package org.example;public class App {
+package org.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
