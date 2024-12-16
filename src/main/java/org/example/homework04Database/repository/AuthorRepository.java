2fe47dffac58996eaccb9644f3fff0fbc99c1d78
elenazmeeva <biertli@bk.ru>
2024-12-16 14:09:33 +0300

    homework database

---
+++ src/main/java/org/example/homework04Database/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package org.example.homework04Database.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository  {
+}
